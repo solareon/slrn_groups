1e0e74298e7ba85828bb5a36261178c790d3443d
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 10:20:30 +0000

    chore: bump manifest version to v1.0.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ use_fxv2_oal 'yes'
 title 'slrn_groups'
 description 'A Groups app made for LB-Phone'
 author 'solareon.'
-version '1.0.1'
+version '1.0.3'
 repository 'https://github.com/solareon/slrn_groups'
 
 client_scripts {
