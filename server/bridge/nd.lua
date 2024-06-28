40e7ea9b41ceb171df3fce9bf3fc15e73fa78c5d
solareon <769465+solareon@users.noreply.github.com>
2024-06-28 20:55:01 +0200

    feat(bridge): bridge support

---
+++ server/bridge/nd.lua
@@ -0,0 +1,17 @@
+if not lib.checkDependency('ND_Core', '2.0.0') then return end
+
+NDCore = {}
+
+lib.load('@ND_Core.init')
+
+function GetPlayer(id)
+    return NDCore.getPlayer(id)
+end
+
+---Get the player data from the source
+---@param source number
+---@return string
+function GetPlayerName(source)
+    local player = GetPlayer(source)
+    return player.fullname
+end
